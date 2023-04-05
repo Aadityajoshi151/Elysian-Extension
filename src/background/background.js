d49ba1dce8cc7415f87683b163edf6501c7699d4
Aadityajoshi151 <aadityajoshi151@gmail.com>
2023-04-05 22:51:40 +0530

    add post request to onCreated bookmark event

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,12 @@
 chrome.bookmarks.onCreated.addListener(function(id, info) {
-    console.log(id)
-    console.log(info)
+    console.log(info);
+    fetch("http://localhost:3000/add_bookmark", {
+    method: "POST",
+    body: JSON.stringify(info),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+   });
   });
 
 chrome.bookmarks.onChanged.addListener(function(id, info) {
