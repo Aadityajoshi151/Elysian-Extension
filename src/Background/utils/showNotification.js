5901287327906edac568d9bc1ad8cdc232f7934e
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-09-23 17:33:41 +0530

    refactoring notification files code

--- src/Background/utils/showNotification.js
+++ src/Background/utils/showNotification.js
@@ -1,8 +1,8 @@
 export function showNotification(title, message) {
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: chrome.runtime.getURL('assets/Elysian_Logo.png'),
-      title: title,
-      message: message
-    }, function () { });
-  }
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: chrome.runtime.getURL('assets/Elysian_Logo.png'),
+    title: title,
+    message: message
+  }, function () { });
+}
